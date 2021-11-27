1273d22145cecc0141c0dd50908b4c2b33fd58cd
ksrnnb <enrico.fermi.phys@gmail.com>
2021-11-28 08:27:54 +0900

    feat: add teachers endpoint #14

--- app/route/route.go
+++ app/route/route.go
@@ -1,13 +1,28 @@
 package route
 
 import (
-	"net/http"
-
+	"github.com/ksrnnb/learn-ddd/infrastructure"
+	"github.com/ksrnnb/learn-ddd/presentation"
+	"github.com/ksrnnb/learn-ddd/presentation/response"
+	"github.com/ksrnnb/learn-ddd/usecase"
 	"github.com/labstack/echo/v4"
 )
 
+var teacherController *presentation.TeacherController
+
+func init() {
+	// TODO: もう少し良い渡し方がないか検討
+	// di container...
+	u := usecase.NewGetTeachersUsecase(infrastructure.NewTeacherRepository())
+	teacherController = presentation.NewTeacherController(*u)
+}
+
 func RegisterRoute(e *echo.Echo) {
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, world!")
-	})
+	e.GET("/teachers", echoWrapper(teacherController.GetTeachers()))
+}
+
+func echoWrapper(res response.Response) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		return c.JSON(res.Code(), res.Body())
+	}
 }
