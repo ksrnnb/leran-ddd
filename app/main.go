f31d35a8eca35ff328df6ac9983ee2afdaa28f3e
ksrnnb <enrico.fermi.phys@gmail.com>
2021-11-30 07:00:08 +0900

    feat: add env file #14

--- app/main.go
+++ app/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/ksrnnb/learn-ddd/route"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
 
 	route.RegisterRoute(e)
