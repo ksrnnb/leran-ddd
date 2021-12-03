80a6d288730cb31396eb2e457d4ab7ce29cb9a2a
ksrnnb <enrico.fermi.phys@gmail.com>
2021-12-03 18:51:05 +0900

    feat: update presentation layer #20

--- app/presentation/club_controller.go
+++ app/presentation/club_controller.go
@@ -7,13 +7,18 @@ import (
 	"github.com/ksrnnb/learn-ddd/presentation/request"
 	"github.com/ksrnnb/learn-ddd/presentation/response"
 	"github.com/ksrnnb/learn-ddd/usecase"
+	"github.com/ksrnnb/learn-ddd/usecase/input"
 )
 
 type ClubController struct {
-	getClubsUsecase usecase.GetClubsUsecase
+	getClubsUsecase   usecase.GetClubsUsecase
+	createClubUsecase usecase.CreateClubUsecase
 }
 
-func NewClubController(getClubsUsercase usecase.GetClubsUsecase) *ClubController {
+func NewClubController(
+	getClubsUsercase usecase.GetClubsUsecase,
+	createClubUsecase usecase.CreateClubUsecase,
+) *ClubController {
 	return &ClubController{getClubsUsecase: getClubsUsercase}
 }
 
@@ -26,5 +31,18 @@ func (c ClubController) GetClubs(req *request.GetClubsRequest) response.Response
 	}
 
 	res := response.NewGetClubsResponse(http.StatusOK, out.Clubs)
+	return res
+}
+
+func (c ClubController) CreateClub(req *request.CreateClubRequest) response.Response {
+	in := input.NewCreateClubInput(req.Name, req.StudentIds, req.TeacherId)
+	out, err := c.createClubUsecase.CreateClub(in)
+
+	if err != nil {
+		logger.Println(err.Error())
+		return response.NewErrorResponse(err.Code(), err.Error())
+	}
+
+	res := response.NewCreateClubResponse(http.StatusCreated, out.Club)
 	return res
 }
