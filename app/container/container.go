27ed13a0a3442fcdf4f9db7d51dd169097bdb5d5
ksrnnb <enrico.fermi.phys@gmail.com>
2021-12-05 08:13:08 +0900

    feat: add factory dependency #20

--- app/container/container.go
+++ app/container/container.go
@@ -1,6 +1,8 @@
 package container
 
 import (
+	"github.com/ksrnnb/learn-ddd/domain/factory"
+	"github.com/ksrnnb/learn-ddd/domain/repository"
 	"github.com/ksrnnb/learn-ddd/infrastructure"
 	"github.com/ksrnnb/learn-ddd/infrastructure/db"
 	"github.com/ksrnnb/learn-ddd/presentation"
@@ -11,25 +13,38 @@ type Container struct {
 	TeacherController *presentation.TeacherController
 	StudentController *presentation.StudentController
 	ClubController    *presentation.ClubController
+	TeacherRepository repository.TeacherRepositoryInterface
+	StudentRepository repository.StudentRepositoryInterface
+	ClubRepository    repository.ClubRepositoryInterface
 }
 
 // TODO: もう少しいい方法を調べる
 func NewContainer() *Container {
+
+	container := &Container{}
+	container.addRepository()
+
+	u := usecase.NewGetTeachersUsecase(container.TeacherRepository)
+	su := usecase.NewGetStudentsUsecase(container.StudentRepository)
+	gcu := usecase.NewGetClubsUsecase(container.ClubRepository)
+
+	cf := factory.NewClubFactory(container.ClubRepository, container.StudentRepository, container.TeacherRepository)
+	ccu := usecase.NewCreateClubUsecase(container.ClubRepository, *cf)
+
+	container.TeacherController = presentation.NewTeacherController(*u)
+	container.StudentController = presentation.NewStudentController(*su)
+	container.ClubController = presentation.NewClubController(*gcu, *ccu)
+
+	return container
+}
+
+func (c *Container) addRepository() {
 	db, err := db.NewDBClient()
 	if err != nil {
 		panic(err)
 	}
 
-	container := &Container{}
-
-	u := usecase.NewGetTeachersUsecase(infrastructure.NewTeacherRepository(db))
-	container.TeacherController = presentation.NewTeacherController(*u)
-
-	su := usecase.NewGetStudentsUsecase(infrastructure.NewStudentRepository(db))
-	container.StudentController = presentation.NewStudentController(*su)
-
-	cu := usecase.NewGetClubsUsecase(infrastructure.NewClubRepository(db))
-	container.ClubController = presentation.NewClubController(*cu)
-
-	return container
+	c.TeacherRepository = infrastructure.NewTeacherRepository(db)
+	c.StudentRepository = infrastructure.NewStudentRepository(db)
+	c.ClubRepository = infrastructure.NewClubRepository(db)
 }
