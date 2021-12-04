9f9d03890f97f038486e3bae7159055d55d33ff1
ksrnnb <enrico.fermi.phys@gmail.com>
2021-12-05 08:15:41 +0900

    feat: add create club route #20

--- app/route/route.go
+++ app/route/route.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	"net/http"
+
 	"github.com/ksrnnb/learn-ddd/container"
 	"github.com/ksrnnb/learn-ddd/presentation/request"
 	"github.com/labstack/echo/v4"
@@ -12,6 +14,7 @@ func RegisterRoute(e *echo.Echo) {
 	e.GET("/teachers", getTeachers)
 	e.GET("/students", getStudents)
 	e.GET("/clubs", getClubs)
+	e.POST("/clubs", createClub)
 }
 
 func getTeachers(context echo.Context) error {
@@ -32,5 +35,25 @@ func getClubs(context echo.Context) error {
 	c = container.NewContainer()
 	req := request.NewGetClubsRequest()
 	res := c.ClubController.GetClubs(req)
+	return context.JSON(res.Code(), res.Body())
+}
+
+func createClub(context echo.Context) error {
+	c = container.NewContainer()
+	req := new(request.CreateClubRequest)
+
+	if err := context.Bind(req); err != nil {
+		return context.JSON(http.StatusUnprocessableEntity, map[string]string{
+			"message": "invalid request body",
+		})
+	}
+
+	if err := context.Validate(req); err != nil {
+		return context.JSON(http.StatusUnprocessableEntity, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	res := c.ClubController.CreateClub(req)
 	return context.JSON(res.Code(), res.Body())
 }
